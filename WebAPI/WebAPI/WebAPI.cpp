6198b79af972c4a509cf9bb77fe8760c8a6529dc
maishiro <16446436+maishiro@users.noreply.github.com>
2026-01-17 20:38:58 +0900

    init

---
+++ WebAPI/WebAPI/WebAPI.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include "httplib.h"
+#include <nlohmann/json.hpp>
+
+#pragma comment(lib, "cpp-httplib.lib")
+
+using namespace std;
+using json = nlohmann::json;
+
+
+int main()
+{
+	// HTTP
+	httplib::Server svr;
+
+	svr.Get( "/weather", []( const httplib::Request&, httplib::Response& res ) {
+		cout << "GET /weather request." << endl;
+
+		json j;
+		j["locale"] = "Tokyo";
+		j["temperature"] = 23.1;
+
+		res.set_content( j.dump(), "application/json" );
+	} );
+
+
+	svr.Post( "/echo", []( const httplib::Request& req, httplib::Response& res ) {
+		cout << "POST /echo request." << endl;
+
+		json data = json::parse( req.body );
+		cout << data.dump( 2 ) << endl;
+
+		res.set_content( req.body, "application/json" );
+	} );
+
+
+	svr.listen( "0.0.0.0", 8888 );
+	
+	return 0;
+}
