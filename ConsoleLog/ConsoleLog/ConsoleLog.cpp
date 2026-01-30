0b1edb80364448ea8578095bf2664cde3c46bb1d
maishiro <16446436+maishiro@users.noreply.github.com>
2026-01-30 23:20:38 +0900

    spdlog

---
+++ ConsoleLog/ConsoleLog/ConsoleLog.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+
+int main()
+{
+    // Console
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level( spdlog::level::trace );
+    // Rolling file
+    auto max_size = 1048576 * 5;
+    auto max_files = 3;
+    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>( "logs/rotating.txt", max_size, max_files );
+    file_sink->set_level( spdlog::level::trace );
+
+    std::vector<spdlog::sink_ptr> sinks{ console_sink, file_sink };
+    auto logger = std::make_shared<spdlog::logger>( "my_logger", sinks.begin(), sinks.end() );
+    spdlog::set_default_logger( logger );
+
+    logger->info( "はろーわーるど" );
+    spdlog::info( "new logger log message {}", 0 );
+
+    std::string message = std::format( "The answer is {}.", 42 );
+    spdlog::info( message );
+}
